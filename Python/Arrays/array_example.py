4b6844ed56de598ca80430ead8c2c0796b2f147b
Arti Tambade <91612792+ArtiHere@users.noreply.github.com>
2023-10-28 02:01:16 +0530

    Create array_example.py
    python file for array

---
+++ Python/Arrays/array_example.py
@@ -0,0 +1,13 @@
+# Creating an array of integers
+numbers = [10, 20, 30, 40, 50]
+
+# Accessing elements in the array
+print("The element at index 2 is:", numbers[2])
+
+# Modifying an element
+numbers[2] = 35
+
+# Looping through the array and printing its elements
+print("Array elements:")
+for i in range(len(numbers)):
+    print("Element at index", i, ":", numbers[i])
